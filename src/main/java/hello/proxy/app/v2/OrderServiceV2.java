b70ef3ad2d9f020a985591cb1f85ca3c33c9760c
JinHawn <b980131@gmail.com>
2022-06-01 16:07:02 +0900

    second commit interface & concrete class proxy

--- src/main/java/hello/proxy/app/v2/OrderServiceV2.java
+++ src/main/java/hello/proxy/app/v2/OrderServiceV2.java
@@ -1,9 +1,6 @@
 package hello.proxy.app.v2;
 
-import hello.proxy.app.v1.OrderRepositoryV1;
-
 public class OrderServiceV2 {
-
     private final OrderRepositoryV2 orderRepository;
 
     public OrderServiceV2(OrderRepositoryV2 orderRepository) {
